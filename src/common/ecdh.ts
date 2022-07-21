cc51584efa2c1a20f8e3c83a454dd3bf2c196399
Karry Nguyen <nguyenvanlong12092000@gmail.com>
2022-07-21 09:15:56 +0700

    Lay khoa bi mat

--- src/common/ecdh.ts
+++ src/common/ecdh.ts
@@ -1,27 +1,30 @@
+import { getCurves } from 'crypto';
 import { createECDH, createHash, ECDH } from 'crypto'
 import { Key } from '../user/interfaces/key.interface'
+import { Log } from './logger';
 
 export class ECDHService {
   private readonly ecdh: ECDH;
 
   constructor() {
+    getCurves().forEach((curve) => {
+      Log.info(ECDHService.name, `Found curve ${curve}`);
+    });
     this.ecdh = createECDH('secp256k1');
   }
 
   generateKeys(): Key {
-    this.ecdh.generateKeys();
+    this.ecdh.generateKeys('base64url', 'compressed');
     return {
-      publicKey: this.ecdh.getPublicKey().toString('hex'),
-      privateKey: this.ecdh.getPrivateKey().toString('hex'),
+      publicKey: this.ecdh.getPublicKey('base64url', 'compressed'),
+      privateKey: this.ecdh.getPrivateKey('base64url'),
     };
   }
 
   static generateSharedSecret(ourPrivateKey: string, theirPublicKey: string) {
     const ecdh = createECDH('secp256k1');
-    ecdh.setPrivateKey(Buffer.from(ourPrivateKey, 'hex'));
-    return ecdh
-      .computeSecret(Buffer.from(theirPublicKey, 'hex'))
-      .toString('hex');
+    ecdh.setPrivateKey(Buffer.from(ourPrivateKey, 'base64url'));
+    return ecdh.computeSecret(theirPublicKey, 'base64url', 'base64url')
   }
 
   static generateFinalKey(
